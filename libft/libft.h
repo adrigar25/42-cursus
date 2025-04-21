231a49105e8211541de03ba71051708d627bc7ec
Adrián <adrigar250503@gmail.com>
2025-04-21 18:03:34 +0200

    Correciones y lstmap

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 11:51:51 by agarcia           #+#    #+#             */
-/*   Updated: 2025/04/21 16:06:32 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/04/21 17:48:12 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,14 +59,13 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
-int					ft_lstsize(t_list *lst);
-t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
-void				ft_lstdelone(t_list *lst, void (*del)(void *));
+t_list				*ft_lstlast(t_list *lst);
+int				ft_lstsize(t_list *lst);
 void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
--- libft/main.c
+++ libft/main.c
@@ -1,5 +1,4 @@
 #include "libft.h"
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,66 +16,56 @@ void	ft_result(char *expected, char *actual)
 		printf("\033[0;31m[FAIL]\033[0m\n");
 }
 
-char	my_function(unsigned int i, char c)
+void *test_function_map(void *content)
 {
-	return (c + i);
+	char *str = (char *)content;
+	unsigned int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+			str[i] = (str[i] + i) % 127;
+		if (str[i] < 32)
+			str[i] += 32;
+		i++;
+	}
+	return (content);
 }
 
-void	my_function2(unsigned int i, char *c)
+void	free_content(void *content)
 {
-	if (*c >= 32 && *c <= 126) // Solo modifica caracteres imprimibles
-		*c = (*c + i) % 127;  
-			// Asegura que el resultado esté en el rango ASCII
-	if (*c < 32)               // Si cae fuera del rango imprimible, ajusta
-		*c += 32;
+	free(content);
 }
 
 int	main(void)
 {
-	char	src[100] = "Hello World";
-	char	*str;
-	char	**result;
+	t_list	*lst;
+	t_list	*new_node;
+	t_list	*mapped_lst;
 
-	// Pruebas para ft_striteri
-	ft_striteri(src, my_function2);
-	printf("ft_striteri: %s\n", src);
-	printf("Expected: %s\n", "Hfnos!Xpsme");
-	ft_result("Hfnos!Xpsme", src);
-	// Pruebas para ft_split
-	printf("\n\033[0;33mTesting ft_split:\033[0m\n");
-	result = ft_split("Hello World", ' ');
-	printf("ft_split: %s\n", result[0]);
+	printf("\n\033[0;33mTesting ft_lstmap:\033[0m\n");
+
+	// Create the original list
+	lst = ft_lstnew(strdup("Hello"));
+	new_node = ft_lstnew(strdup("World"));
+	ft_lstadd_back(&lst, new_node);
+
+	// Apply ft_lstmap
+	mapped_lst = ft_lstmap(lst, test_function_map, free_content);
+
+	// Test the first node
+	printf("ft_lstmap (first node): %s\n", (char *)mapped_lst->content);
 	printf("Expected: %s\n", "Hello");
-	ft_result("Hello", result[0]);
-	// Pruebas para ft_strdup
-	printf("\n\033[0;33mTesting ft_strdup:\033[0m\n");
-	str = ft_strdup("Hello World");
-	printf("ft_strdup: %s\n", str);
-	printf("Expected: %s\n", "Hello World");
-	ft_result("Hello World", str);
-	// Pruebas para ft_substr
-	printf("\n\033[0;33mTesting ft_substr:\033[0m\n");
-	str = ft_substr("Hello World", 0, 5);
-	printf("ft_substr: %s\n", str);
-	printf("Expected: %s\n", "Hello");
-	ft_result("Hello", str);
-	// Pruebas para ft_strtrim
-	printf("\n\033[0;33mTesting ft_strtrim:\033[0m\n");
-	str = ft_strtrim("   Hello World   ", " ");
-	printf("ft_strtrim: %s\n", str);
-	printf("Expected: %s\n", "Hello World");
-	ft_result("Hello World", str);
-	// Pruebas para ft_strlcpy
-	printf("\n\033[0;33mTesting ft_strlcpy:\033[0m\n");
-	str = malloc(20);
-	if (str == NULL)
-	{
-		printf("Memory allocation failed\n");
-		return (1);
-	}
-	ft_strlcpy(str, "Hello World", 20);
-	printf("ft_strlcpy: %s\n", str);
-	printf("Expected: %s\n", "Hello World");
-	ft_result("Hello World", str);
+	ft_result("Hello", (char *)mapped_lst->content);
+
+	// Test the second node
+	printf("ft_lstmap (second node): %s\n", (char *)mapped_lst->next->content);
+	printf("Expected: %s\n", "World");
+	ft_result("World", (char *)mapped_lst->next->content);
+
+	// Free allocated memory
+	ft_lstclear(&lst, free_content);
+	ft_lstclear(&mapped_lst, free_content);
+
 	return (0);
 }
