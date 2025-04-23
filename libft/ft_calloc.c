f962ff6a93c23a5062d23c092c04b1c349f5c765
Adrián <adrigar250503@gmail.com>
2025-04-23 16:20:14 +0200

    Más correciones y tests

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,18 +6,19 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:53:44 by agarcia           #+#    #+#             */
-/*   Updated: 2025/04/15 17:39:35 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/04/23 16:19:36 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 	size_t	total_size;
 
-	if (nmemb > 0 && size > 0 && nmemb > SIZE_MAX / size)
+	if (size != 0 && nmemb > SIZE_MAX / size)
 		return (NULL);
 	total_size = nmemb * size;
 	ptr = malloc(total_size);
--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 16:55:29 by agarcia           #+#    #+#             */
-/*   Updated: 2025/04/22 20:42:19 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/04/22 21:19:10 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*res;
-	t_list	*aux;
-	void	*tmp;
+	t_list	*new_list;
+	t_list	*new_elem;
+	void	*new_content;
 
-	if (!lst || !f || !del)
+	if (!lst || !f)
 		return (NULL);
-	res = NULL;
+	new_list = NULL;
 	while (lst)
 	{
-		tmp = f(lst->content);
-		aux = ft_lstnew(tmp);
-		if (!aux)
+		new_content = f(lst->content);
+		new_elem = ft_lstnew(new_content);
+		if (!new_elem)
 		{
-			ft_lstclear(&res, del);
-			del(tmp);
+			del(new_content);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&res, aux);
+		ft_lstadd_back(&new_list, new_elem);
 		lst = lst->next;
 	}
-	return (res);
+	return (new_list);
 }
