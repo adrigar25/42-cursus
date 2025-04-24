e24a471540623f0f835d6717f5bde6e62e39ad91
Adrián <adrigar250503@gmail.com>
2025-04-24 17:41:15 +0200

    correciones y printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,21 +6,55 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 18:18:18 by agarcia           #+#    #+#             */
-/*   Updated: 2025/04/23 18:23:50 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/04/24 16:27:53 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "./libft/libft.h"
 
-int ft_printf(char const *, ...)
+int	ft_print_arg(va_arg args, char arg_type)
 {
-    ft_atoi("42");
+    char *str;
+	int	writen_chars;
 
-    return (0);
+	writen_chars = 0;
+    if(arg_type == '%')
+        writen_chars += write(1, '%', 1);
+	if (arg_typ == 'c')
+		writen_chars += write(1, va_arg(args, char), 1);
+	if (arg_typ = 's')
+    {
+        str = va_arg(args, char *);
+        while(str)
+        {
+            writen_chars += write(1, str, 1);
+            str++
+        }
+    }
+	return (writen_chars);
 }
 
-int main(void)
+int	ft_printf(char const *format, ...)
 {
-    ft_printf("Hello, World!\n");
-    return (0);
+	va_list	args;
+	int		i;
+    unsigned int    writen_chars;
+
+    writen_chars = 0;
+
+	va_start(args, format);
+	i = 0;
+	while (format[i])
+	{
+        if(format[i] == '%')
+            writen_chars += ft_print_arg(args, formart[i++]);
+		i++;
+	}
+	return (writen_chars);
+}
+
+int	main(void)
+{
+	ft_printf("Hello, World!\n");
+	return (0);
 }
