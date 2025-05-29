ee75ad7a5a0019537261ea9ea5e58b76634f4273
Adrián <adrigar250503@gmail.com>
2025-05-29 19:31:21 +0200

    ptr (nil)

--- ft_printf/ft_printf_bonus/bonus/ft_putptr_bonus.c
+++ ft_printf/ft_printf_bonus/bonus/ft_putptr_bonus.c
@@ -6,7 +6,7 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 01:24:39 by agarcia           #+#    #+#             */
-/*   Updated: 2025/05/22 16:25:29 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/05/29 19:31:02 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ int	ft_putptr(void *ptr)
 
 	flags.hash = 1;
 	if (!ptr)
-		return (ft_putstr("0x0"));
+		return (ft_putstr("(nil)"));
 	return (ft_puthex((unsigned long long)ptr, 0, flags));
 }
--- ft_printf/ft_printf_bonus/ft_putptr.c
+++ ft_printf/ft_printf_bonus/ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 01:24:39 by agarcia           #+#    #+#             */
-/*   Updated: 2025/04/30 16:49:57 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/05/29 19:31:10 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	ft_putptr(void *ptr)
 {
 	if (!ptr)
-		return (ft_putstr("0x0"));
+		return (ft_putstr("(nil)"));
 	return (ft_putstr("0x") + ft_puthex((unsigned long long)ptr, 0));
 }
